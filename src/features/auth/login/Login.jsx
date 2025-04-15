5f13dd7984efa38b1ee37e9a5e08dc42c1b82464
“AmroGFarghali” <lordmooora@gmail.com>
2025-04-15 19:21:54 +0200

    finished login design

--- src/features/auth/login/Login.jsx
+++ src/features/auth/login/Login.jsx
@@ -1,56 +1,186 @@
-import React from "react";
+import React, { useState } from "react";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Eye, EyeOff } from "lucide-react";
+
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
+const formSchema = z.object({
+  email: z.string().email({
+    message: "Please enter a valid email address.",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters long.",
+  }),
+});
 
 const Login = () => {
-  return (
-    <div className="flex h-screen ">
-      {/* Left Section: Form */}
-      <div className="flex flex-col justify-center items-center flex-1/2 p-8 bg-white">
-        <h1 className="text-2xl font-bold mb-4">Login</h1>
-        <form className="w-3/4">
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              type="email"
-              id="email"
-              placeholder="Enter your email"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              type="password"
-              id="password"
-              placeholder="Enter your password"
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600"
-          >
-            Login
-          </button>
-        </form>
-      </div>
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
 
-      {/* Right Section: Image */}
-      <div className="flex-1/2  justify-center items-center hidden md:flex">
-        <img
-          className="  md:w-9/12 h-9/12 object-cover border-2 rounded-lg shadow-lg"
-          src="/sign-in.jpg"
-          alt="Sign In"
-        />
+  function onSubmit(values) {
+    console.log(values);
+  }
+  const [showPassword, setIsShowPassword] = useState(false);
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white p-6 md:p-10">
+      <div className="flex flex-col md:flex-row items-center justify-center w-full max-w-6xl space-y-10 md:space-y-0 md:space-x-10">
+        {/* Left Section: Card */}
+        <Card className="w-full max-w-md border-none shadow-none bg-white">
+          <CardHeader>
+            <CardTitle className="text-3xl font-bold">
+              Welcome back 👋
+            </CardTitle>
+            <CardDescription>
+              Please enter your credentials to proceed to AI-Recruiter
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid items-center">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-4"
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input placeholder="placeholder@email.com" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <div className="relative">
+                          <Input
+                            type={showPassword ? "text" : "password"}
+                            placeholder="Password"
+                            {...field}
+                          />
+                          <Button
+                            type="button"
+                            variant="ghost"
+                            size="icon"
+                            className="absolute right-1 top-1/2 transform -translate-y-1/2 h-8 w-8 text-gray-500"
+                            onClick={() => setIsShowPassword(!showPassword)}
+                          >
+                            {showPassword ? (
+                              <EyeOff size={16} />
+                            ) : (
+                              <Eye size={16} />
+                            )}
+                          </Button>
+                        </div>
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <div className="text-right">
+                  <Link
+                    to="/forgot-password"
+                    className="text-sm text-blue-500 hover:underline"
+                  >
+                    Forgot Password?
+                  </Link>
+                </div>
+                <Button className="w-full border border-gray-300" type="submit">
+                  Login
+                </Button>
+              </form>
+            </Form>
+          </CardContent>
+          <div className="flex items-center my-4">
+            <div className="flex-grow border-t border-gray-300"></div>
+            <span className="px-1 text-gray-500 text-sm">OR</span>
+            <div className="flex-grow border-t border-gray-300"></div>
+          </div>
+          {/* Social Login Buttons */}
+          <div className="space-y-2 flex flex-col px-4 mb-4 ">
+            <Button
+              variant="secondary"
+              className="flex items-center justify-center gap-2 "
+            >
+              <svg className="h-5 w-5" viewBox="0 0 24 24">
+                <path
+                  fill="#4285F4"
+                  d="M23.745 12.27c0-.79-.07-1.54-.19-2.27h-11.3v4.51h6.47c-.29 1.48-1.14 2.73-2.4 3.58v3h3.86c2.26-2.09 3.56-5.17 3.56-8.82z"
+                />
+                <path
+                  fill="#34A853"
+                  d="M12.255 24c3.24 0 5.95-1.08 7.93-2.91l-3.86-3c-1.08.72-2.45 1.16-4.07 1.16-3.13 0-5.78-2.11-6.73-4.96h-3.98v3.09c1.97 3.92 6.02 6.62 10.71 6.62z"
+                />
+                <path
+                  fill="#FBBC05"
+                  d="M5.525 14.29c-.25-.72-.38-1.49-.38-2.29s.14-1.57.38-2.29v-3.09h-3.98c-.8 1.61-1.26 3.43-1.26 5.38s.46 3.77 1.26 5.38l3.98-3.09z"
+                />
+                <path
+                  fill="#EA4335"
+                  d="M12.255 5.04c1.77 0 3.35.61 4.6 1.8l3.42-3.42c-2.07-1.94-4.78-3.13-8.02-3.13-4.69 0-8.74 2.7-10.71 6.62l3.98 3.09c.95-2.85 3.6-4.96 6.73-4.96z"
+                />
+              </svg>
+              Sign in with Google
+            </Button>
+            <Button
+              variant="secondary"
+              className="w-full flex items-center justify-center gap-2"
+            >
+              <svg
+                className="h-5 w-5 text-blue-600"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+              >
+                <path d="M22.675 0H1.325C.593 0 0 .593 0 1.325v21.351C0 23.407.593 24 1.325 24h11.495v-9.294H9.693v-3.621h3.127V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12V24h6.116C23.407 24 24 23.407 24 22.675V1.325C24 .593 23.407 0 22.675 0z" />
+              </svg>
+              Sign in with Facebook
+            </Button>
+          </div>
+        </Card>
+
+        {/* Right Section: Image */}
+        <div className="hidden xl:flex justify-center items-center w-full md:w-1/2 border">
+          <img
+            className="h-[90%] object-cover rounded-lg"
+            src="/sign-in.jpg"
+            alt="Login Illustration"
+          />
+        </div>
       </div>
     </div>
   );
