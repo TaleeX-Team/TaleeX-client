ff841f7af8dff548c7dfaf0dbc71866470a2cd79
“AmroGFarghali” <lordmooora@gmail.com>
2025-04-15 09:58:35 +0200

    configured shadcn

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
-    <>
-      <div className="text-3xl text-emerald-800">Hello!</div>
-    </>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
   );
 }
 
