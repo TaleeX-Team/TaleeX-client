2a6325b2258e1739b93882be298797279868a2d1
Muhammed <muhammedrefaat2020@gmail.com>
2025-04-15 14:31:58 +0200

    verification page design.tsx done !

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { Button } from "@/components/ui/button";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./features/auth/login/Login";
 import SignUp from "./features/auth/sign-up/SignUp";
+import {Home} from "@/features/home/Home.jsx";
+import VerificationCodePage from "@/features/auth/verification/VerificationPage.jsx";
 
 function App() {
   const router = createBrowserRouter([
+    {path:"/",element: <Home />},
     { path: "/login", element: <Login /> },
-    { path: "/sign-up", element: <SignUp /> },
+    { path: "/sign-up", element: <SignUp /> },{
+    path: "/verify",element:<VerificationCodePage/>,
+    }
   ]);
   return <RouterProvider router={router}></RouterProvider>;
 }
